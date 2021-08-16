851e9b5ef0bdc7631ab68344e47c03756843c998
Dinesh Rout <rddinesh779@gmail.com>
2021-08-16 21:48:09 +0530

    is triangle logic added

--- app.js
+++ app.js
@@ -23,3 +23,36 @@ const switchTab = (i) => {
 };
 
 window.addEventListener('load', switchTab(0));
+
+let firstAngle = document.querySelector('#first-angle');
+let secondAngle = document.querySelector('#second-angle');
+let thirdAngle = document.querySelector('#third-angle');
+let angleBtn = document.querySelector('#angle-btn');
+let angleOutput = document.querySelector('#angle-output');
+
+const isTriangle = (angle1, angle2, angle3) => {
+  angle1 = Number(angle1);
+  angle2 = Number(angle2);
+  angle3 = Number(angle3);
+  let sum = angle1 + angle2 + angle3;
+  console.log(sum);
+  if (sum === 180) {
+    angleOutput.style.color = 'green';
+    angleOutput.innerHTML = '<h2>Yayy!! This angles make a &#x25B3;</h2>';
+  } else {
+    angleOutput.style.color = 'red';
+    angleOutput.innerHTML =
+      '<h2>No!! Thease angles do not form a &#x25B3;</h2>';
+  }
+};
+
+angleBtn.addEventListener('click', (e) => {
+  e.preventDefault();
+  if (
+    firstAngle.value !== '' &&
+    secondAngle.value !== '' &&
+    thirdAngle.value !== ''
+  ) {
+    isTriangle(firstAngle.value, secondAngle.value, thirdAngle.value);
+  }
+});
